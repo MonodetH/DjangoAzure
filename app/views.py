ccf6eb5c50e0ceeb78c8a2e6128b2f83ccd36ace
MonodetH <monodeth@gmail.com>
2017-06-10 17:15:04 -0400

    test post

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 Definition of views.
 """
 
-import httplib, urllib, base64, json,requests
+import httplib, urllib, base64, json, requests
 import urllib2
 from django.shortcuts import render
 from django.http import HttpRequest
@@ -14,26 +14,20 @@ def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
 
-    # headers = {
-    #     # Request headers
-    #     'Content-Type': 'application/json',
-    #     'Ocp-Apim-Subscription-Key': '99fd5eb582914f7a8595822812988b94',
-    # }
-    #
-    # params = urllib.urlencode({
-    # })
-    #
-    # body = {"documents":[{"id":"1", "text":"pantalla mala, 16Gb de memoria"}]}
-    #
-    #
-    # conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    # conn.request("POST", "/text/analytics/v2.0/keyPhrases" % params, json.dumps(body), headers)
-    # response = conn.getresponse()
-    # data = response.read()
-    # print(data)
-    # conn.close()
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': '99fd5eb582914f7a8595822812988b94',
+    }
 
-    #r = requests.post("https://westus.api.cognitive.microsoft.com", data={"documents":[{"id":"1", "text":"pantalla mala, 16Gb de memoria"}]'})
+    body = {"documents":[{"id":"1", "text":"pantalla mala, 16Gb de memoria"}]}
+
+    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/text/analytics/v2.0/keyPhrases", json.dumps(body), headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
 
     return render(
         request,
